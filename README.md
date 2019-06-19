6b263e20d673065b96df0ede0a3bed4a8df900d0
Rory Lindsay <rorywork95@outlook.com>
2019-06-19 21:09:24 +0000

    Prototype working, ready for more in depth layout and styling - still known bug with preloader so removed from code for now

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Pokemon Master
 
-Pokemon Master will test your knowledge of the different types of Pokemon. Everyone knows that water beats fire but do you know the rest. Find out here and become a Pokemon Master!
+Pokemon Master will test your knowledge of the Pokemon in generation 1. You have to work out which Pokemon is is based on a series of clues. Find out here and become a Pokemon Master!
 
 ## UX
 
-The goal of Pokemon Master is to provide a fun, entertaining way of testing your knowledge of the different types of Pokemon. Pokemon each have a type and certain types are strong or weak versus others. It is
+The goal of Pokemon Master is to provide a fun, entertaining way of testing your knowledge of the differen Pokemon. You are given an image fo the Pokemon from behind and up to three clues to work it out. It is
 perfect for procrastinating at work, keeping the kids entertained or providing your daily dose of nostaligia. It is designed for Pokemon fans of all ages. 
 
 #### Player Goals
@@ -29,7 +29,7 @@ perfect for procrastinating at work, keeping the kids entertained or providing y
 As a user, I want the game to load quickly so I can start playing.  
 As a user, I want to play with lots of different Pokemon.  
 As a user, I want to see how I am doing in the game and keep track of my score.  
-As a user, I want to be challenged so that I improve my knowledge of the types of Pokemon.  
+As a user, I want to be challenged so that I improve my knowledge of the Gen 1 Pokemon.
 As a user, I want the game to bw fun to play.  
 As a user, I would appreciate the game being free to play.  
 As a returning player, I would like there to be enough challenge so that there is some replay value within the game.
--- js/pokescript.js
+++ js/pokescript.js
@@ -2,37 +2,37 @@
 
 // Preloader
 
-$(window).on('load', function(){
-    $('.preloader').addClass('complete')
-})
+// $(window).on('load', function() {
+//     $('.preloader').addClass('complete')
+// })
 
 
 
 
 
-var selectedPokemon  // Initialise variable to hold random Pokemon ID
+var selectedPokemon // Initialise variable to hold random Pokemon ID
 
-var  name
-            
+var name
+
 var frontImage
-        
+
 var backImage
-        
-var typeOfPokemon 
-        
+
+var typeOfPokemon
+
 var abilityOne
-        
+
 var weight
 
 var looper;
 
 var degrees = 0;
 
-var numAnswers = 5;      // How many possible answers to display
+var numAnswers = 5; // How many possible answers to display
 
-var candidateAnswerPokemonNames = [];       // Initialise array for Pokemon names
+var candidateAnswerPokemonNames = []; // Initialise array for Pokemon names
 
-var ansHighlightPos      // Answer list highlight position 
+var ansHighlightPos // Answer list highlight position 
 
 var userScore;
 
@@ -47,384 +47,385 @@ var gameStatus = ""; // Holds the status of the game i.e what page/section it is
 
 
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
-    
- });
+$(document).ready(function() {
+    console.log("ready!");
+
+});
 
 
 
 
-function firstClue(){
-    
-        let pokeapiUrl = `https://pokeapi.co/api/v2/pokemon/${(Math.floor(Math.random() * 151) + 1).toString()}`; // Generates a random pokemon id from the API
-        $("#scoreScreen").empty();
-        $("#mainBody").empty();
-        
-        writeText(" Below is an image from the back of the Pokemon that you have to guess... Click A if you need another clue.")
-    
-        console.log(pokeapiUrl);
-    
-         $.getJSON(pokeapiUrl).done(function(data){
-            console.log(data);
-    
+function firstClue() {
+
+    let pokeapiUrl = `https://pokeapi.co/api/v2/pokemon/${(Math.floor(Math.random() * 151) + 1).toString()}`; // Generates a random pokemon id from the API
+    $("#scoreScreen").empty();
+    $("#mainBody").empty();
+
+    writeText(" Below is an image from the back of the Pokemon that you have to guess... Click A if you need another clue.")
+
+    console.log(pokeapiUrl);
+
+    $.getJSON(pokeapiUrl).done(function(data) {
+        console.log(data);
+
         selectedPokemon = data.id;
-            console.log (selectedPokemon);
-    
+        console.log(selectedPokemon);
+
         name = data.species.name.charAt(0).toUpperCase() + data.species.name.slice(1);
-            console.log(name);
-            
+        console.log(name);
+
         frontImage = data.sprites.front_default;
         console.log(frontImage)
-        
+
         backImage = data.sprites.back_default;
-        
+
         typeOfPokemon = data.types[0].type.name;
-            console.log(typeOfPokemon);
-        
+        console.log(typeOfPokemon);
+
         abilityOne = data.abilities[0].ability.name;
-            console.log(abilityOne);
-        
+        console.log(abilityOne);
+
         weight = data.weight;
-            console.log(weight);
-            
-        backImagePokemon();    
-     }).fail(function(){
-         console.log("Request to Pokeapi failed.")
-     }).always(function(){
+        console.log(weight);
+
+        backImagePokemon();
+    }).fail(function() {
+        console.log("Request to Pokeapi failed.")
+    }).always(function() {
         console.log("Request to Pokeapi was successful.");
-        gameStatus ="firstclueloaded"
-     });    
-        
+        gameStatus = "firstclueloaded"
+    });
+
 }
 
 
 
-function frontImagePokemon(){
-    
+function frontImagePokemon() {
+
     let frontSprite = $("<div>").html("<img src=" + frontImage + "></img>");
     frontSprite.appendTo("#front-image-div");
     console.log(frontImage)
-    
-    
+
+
 }
 
-function backImagePokemon(){
-    
+function backImagePokemon() {
+
     let backSprite = $("<div>").html("<img src=" + backImage + "></img>");
-    
+
     $("#loading-image").empty();
-    
+
     backSprite.appendTo("#loading-image");
     console.log(backImage)
-    
-    
+
+
 }
 
-function secondClue(){
-    
+function secondClue() {
+
     let pokemonType = $("<p>").html(`This is a ${typeOfPokemon} Pokemon. Click A if you need another clue.`)
-        $("#mainBody").empty();
-               pokemonType.appendTo("#mainBody");
-               
-    gameStatus ="secondclueloaded"
-    
-    
+    $("#mainBody").empty();
+    pokemonType.appendTo("#mainBody");
+
+    gameStatus = "secondclueloaded"
+
+
 }
 
-function thirdClue(){
-    
+function thirdClue() {
+
     let pokemonAbility = $("<p>").html(`This Pokemon has the ability ${abilityOne}. Click A if you need another clue.`)
-               pokemonAbility.appendTo("#mainBody");
+    pokemonAbility.appendTo("#mainBody");
 
-    gameStatus ="thirdclueloaded"
-    
+    gameStatus = "thirdclueloaded"
+
 }
 
-function fourthClue(){
-    
-    let pokemonWeight = $("<p>").html(`This Pokemon weighs ${weight} Pokegrams. That is all the clues. Now you need to choose the Pokemon by pressing button B`)
-               pokemonWeight.appendTo("#mainBody");
+function fourthClue() {
 
-    gameStatus ="fourthclueloaded"
+    let pokemonWeight = $("<p>").html(`This Pokemon weighs ${weight} Pokegrams. That is all the clues. Now you need to choose the Pokemon by pressing button B`)
+    pokemonWeight.appendTo("#mainBody");
+
+    gameStatus = "fourthclueloaded"
 
 }
 
 
 
 function getPokedex() {
-    
+
     // get the list of Pokemon
     var pokeapiURL = "https://pokeapi.co/api/v2/generation/1";
-    
+
     $.getJSON(pokeapiURL).done(function(data) {
         console.log(data);
         // if required data element is an array then loop through each of the entries
-        
-        
-        
-        $.each(data.pokemon_species, function(index,pokemon) {
+
+
+
+        $.each(data.pokemon_species, function(index, pokemon) {
             console.log(pokemon.name);
             var name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1); // String manipulation to Capitalise
             var url = pokemon.url;
             // get the Index of the specific Pokemon via substring on the url loking for pos "s/" and final "/"
             var pIndex = url.substring(
-                url.lastIndexOf("s/") + 2, 
+                url.lastIndexOf("s/") + 2,
                 url.lastIndexOf("/")
             );
-        var pokedex = $("<p>").html(`Pokemon is ${name}`)
-              pokedex.appendTo("#pokedex-div");
+            var pokedex = $("<p>").html(`Pokemon is ${name}`)
+            pokedex.appendTo("#pokedex-div");
         });
     });
-}    
+}
 
 
 
 function getRandomCandidateAnswers() {
-    
-    var pokemonAnswerIDs = [];            // Initialise array to generate random pokemon IDs
-    
+
+    var pokemonAnswerIDs = []; // Initialise array to generate random pokemon IDs
+
     // Loop numAnswers - 1 times to populate array. (minus 1 to accomodate an element for the correct answer)
     for (i = 0; i < numAnswers - 1; i++) {
-        
+
         var candidateAnswerID = Math.floor(Math.random() * 151) + 1;
-        pokemonAnswerIDs.indexOf(candidateAnswerID) === -1 && candidateAnswerID != selectedPokemon ? pokemonAnswerIDs.push(candidateAnswerID) :  i-- ;
+        pokemonAnswerIDs.indexOf(candidateAnswerID) === -1 && candidateAnswerID != selectedPokemon ? pokemonAnswerIDs.push(candidateAnswerID) : i--;
     }
-    
+
     // Finally add the correct Pokemon ID to the array
     pokemonAnswerIDs.push(selectedPokemon);
-    
-    console.log(pokemonAnswerIDs);    
-    
-    // Code to build array of possible answers from the API pokemon names seleced from the random IDs above
-    
-    var pokeapiURL = "https://pokeapi.co/api/v2/generation/1";      // API call to return full Pokedex
 
-    
+    console.log(pokemonAnswerIDs);
+
+    // Code to build array of possible answers from the API pokemon names seleced from the random IDs above
+
+    var pokeapiURL = "https://pokeapi.co/api/v2/generation/1"; // API call to return full Pokedex
+
+
     // JSON execute to return API data to variable data
     $.getJSON(pokeapiURL).done(function(data) {
         //console.log(data);
-       
+
         // Loop through species element to retrieve Pokemon name and unique url       
-        $.each(data.pokemon_species, function(index,pokemon) {
+        $.each(data.pokemon_species, function(index, pokemon) {
             var name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1); // String manipulation to Capitalise Pokemon name
             var url = pokemon.url;
             // get the Index of the specific Pokemon via substring on the url looking for pos "s/" and final "/"
             var pIndex = url.substring(
-                url.lastIndexOf("s/") + 2, 
+                url.lastIndexOf("s/") + 2,
                 url.lastIndexOf("/")
             );
-            
+
             // If the PokemonAnswerIDs array contains the current pIndex from the API call, then push the name onto the array
-            
-            pokemonAnswerIDs.indexOf(parseInt(pIndex)) >= 0 ? candidateAnswerPokemonNames.push(name) : console.log() ;
+
+            pokemonAnswerIDs.indexOf(parseInt(pIndex)) >= 0 ? candidateAnswerPokemonNames.push(name) : console.log();
         });
-        
-    console.log(candidateAnswerPokemonNames);
-    
-    ansHighlightPos = 1;
-    
-     $("#mainBody").empty();
-    
-    var listHtml = "<ul class='navmenu'>";
-    
-    $.each(candidateAnswerPokemonNames, function(index,pokemonName) {
-        listHtml += "<li id='listPos" + (index + 1).toString() + "'><a href='#'>" + pokemonName + "</a></li>";
-    });
-    
-    listHtml += '</ul>';
-    $('#mainBody').append(listHtml);
-    
-    $('.navmenu').on('click','li', function(){
-        $('.navmenu li.active').removeClass('active');
-        $(this).addClass('active');
-    })
-    
-    $("#listPos1").css("background-color", "#555");
+
+        console.log(candidateAnswerPokemonNames);
+
+        ansHighlightPos = 1;
+
+        $("#mainBody").empty();
+
+        var listHtml = "<ul class='navmenu'>";
+
+        $.each(candidateAnswerPokemonNames, function(index, pokemonName) {
+            listHtml += "<li id='listPos" + (index + 1).toString() + "'><a href='#'>" + pokemonName + "</a></li>";
+        });
+
+        listHtml += '</ul>';
+        $('#mainBody').append(listHtml);
+
+        $('.navmenu').on('click', 'li', function() {
+            $('.navmenu li.active').removeClass('active');
+            $(this).addClass('active');
+        })
+
+        $("#listPos1").css("background-color", "#555");
 
 
-    }).fail(function(){
-         console.log("Request to Pokeapi failed.")
-     }).always(function(){
+    }).fail(function() {
+        console.log("Request to Pokeapi failed.")
+    }).always(function() {
         console.log("Request to Pokeapi was successful.");
-        gameStatus ="answerlist"
-     });   
-    
+        gameStatus = "answerlist"
+    });
+
 }
 
 
-function getElectrode(){
-    
-     let pokeapiUrl = "https://pokeapi.co/api/v2/pokemon/electrode"
-    
-        console.log(pokeapiUrl);
-    
-         $.getJSON(pokeapiUrl).done(function(data){
-            console.log(data);
-    
+function getElectrode() {
+
+    let pokeapiUrl = "https://pokeapi.co/api/v2/pokemon/electrode"
+
+    console.log(pokeapiUrl);
+
+    $.getJSON(pokeapiUrl).done(function(data) {
+        console.log(data);
+
         frontImage = data.sprites.front_default;
         console.log(frontImage)
-        
+
         $("#loading-image").empty();
-        
+
         $('#loading-image').prepend("<img id='img1' src=" + frontImage + "></img>")
-        
-      
-     }).fail(function(){
-         console.log("Request to Pokeapi failed.")
-     }).always(function(){
+
+
+    }).fail(function() {
+        console.log("Request to Pokeapi failed.")
+    }).always(function() {
         console.log("Request to Pokeapi was successful.");
-        gameStatus ="loadingcompleted"
-     });  
-    
+        gameStatus = "loadingcompleted"
+    });
+
 }
 
-function writeText(message){
-    
+function writeText(message) {
+
     let screenMessage = $("<p>").html(message)
-    
+
     $("#mainBody").empty();
-    
+
     screenMessage.appendTo("#mainBody")
 }
 
-function loadingScreen(){
-    
+function loadingScreen() {
+
     console.log("Testing")
-    
+
     writeText("Loading....");
     getElectrode();
     userScore = 0;
     gameStatus = "loadingcompleted"
-    
+
 }
 
-function rotateAnimation(el,speed){
-    
-	var elem = document.getElementById(el);
-	if(navigator.userAgent.match("Chrome")){
-		elem.style.WebkitTransform = "rotate("+degrees+"deg)";
-	} else if(navigator.userAgent.match("Firefox")){
-		elem.style.MozTransform = "rotate("+degrees+"deg)";
-	} else if(navigator.userAgent.match("MSIE")){
-		elem.style.msTransform = "rotate("+degrees+"deg)";
-	} else if(navigator.userAgent.match("Opera")){
-		elem.style.OTransform = "rotate("+degrees+"deg)";
-	} else {
-		elem.style.transform = "rotate("+degrees+"deg)";
-	}
-	looper = setTimeout('rotateAnimation(\''+el+'\','+speed+')',speed);
-	degrees++;
-	if(degrees > 359){
-		degrees = 1;
-	}
-	
+function rotateAnimation(el, speed) {
+
+    var elem = document.getElementById(el);
+    if (navigator.userAgent.match("Chrome")) {
+        elem.style.WebkitTransform = "rotate(" + degrees + "deg)";
+    }
+    else if (navigator.userAgent.match("Firefox")) {
+        elem.style.MozTransform = "rotate(" + degrees + "deg)";
+    }
+    else if (navigator.userAgent.match("MSIE")) {
+        elem.style.msTransform = "rotate(" + degrees + "deg)";
+    }
+    else if (navigator.userAgent.match("Opera")) {
+        elem.style.OTransform = "rotate(" + degrees + "deg)";
+    }
+    else {
+        elem.style.transform = "rotate(" + degrees + "deg)";
+    }
+    looper = setTimeout('rotateAnimation(\'' + el + '\',' + speed + ')', speed);
+    degrees++;
+    if (degrees > 359) {
+        degrees = 1;
+    }
+
 }
 
 
 function showGameInstructions() {
-    
+
     $("#scoreScreen").empty();
-    
-    rotateAnimation('img1',10);
+
+    rotateAnimation('img1', 10);
     writeText('Here are the instructions on how to play the game. The back of a pokemon will appear on screen, press the B button to get clues about the pokemon, when you are ready to guess press the A button')
     gameStatus = "instructionsloaded"
 }
 
-function listDown(){
-    $("#listPos" + ansHighlightPos.toString()).css("background-color", "#f1f1f1");    
-    ansHighlightPos ++;
-    if(ansHighlightPos == numAnswers + 1) ansHighlightPos = 1;
-    $("#listPos" + ansHighlightPos.toString()).css("background-color", "#555");    
+function listDown() {
+    $("#listPos" + ansHighlightPos.toString()).css("background-color", "#f1f1f1");
+    ansHighlightPos++;
+    if (ansHighlightPos == numAnswers + 1) ansHighlightPos = 1;
+    $("#listPos" + ansHighlightPos.toString()).css("background-color", "#555");
 }
 
-function listUp(){
-    $("#listPos" + ansHighlightPos.toString()).css("background-color", "#f1f1f1");    
-    ansHighlightPos --;
-    if(ansHighlightPos == 0) ansHighlightPos = numAnswers;
-    $("#listPos" + ansHighlightPos.toString()).css("background-color", "#555");    
+function listUp() {
+    $("#listPos" + ansHighlightPos.toString()).css("background-color", "#f1f1f1");
+    ansHighlightPos--;
+    if (ansHighlightPos == 0) ansHighlightPos = numAnswers;
+    $("#listPos" + ansHighlightPos.toString()).css("background-color", "#555");
 }
 
 
 function checkAnswer() {
-  
+
     var isCorrect = ((name == candidateAnswerPokemonNames[ansHighlightPos - 1]) ? 'correct' : 'incorrect');
     $("#mainBody").empty();
     writeText("You selected " + candidateAnswerPokemonNames[ansHighlightPos - 1] + ". That is " + isCorrect);
-    
+
     $("#scoreScreen").empty();
-    
-    
-    if(name == candidateAnswerPokemonNames[ansHighlightPos - 1]) {
-        userScore ++ ;
+
+
+    if (name == candidateAnswerPokemonNames[ansHighlightPos - 1]) {
+        userScore++;
         var screenScore = $("<p>").html(`${userScore} points. Click A for next pokemon`)
         screenScore.appendTo("#scoreScreen");
         gameStatus = "instructionsloaded";
-    } else {
+    }
+    else {
         var screenScore = $("<p>").html(`Game over you scored ${userScore} points. Click A to play again.`)
         screenScore.appendTo("#scoreScreen");
         userScore = 0
         gameStatus = "instructionsloaded";
     }
-    
-    candidateAnswerPokemonNames = [];       // Reset the array of possible answers
-        
+
+    candidateAnswerPokemonNames = []; // Reset the array of possible answers
+
 }
 
-function buttonAHandler(){
-    
+function buttonAHandler() {
+
     console.log(gameStatus);
-   if (gameStatus == ""){
-        
+    if (gameStatus == "") {
+
         loadingScreen();
-        
+
     }
-    
-    else if (gameStatus == "loadingcompleted"){
-        
+
+    else if (gameStatus == "loadingcompleted") {
+
         showGameInstructions();
-        
+
     }
 
-    else if (gameStatus == "instructionsloaded"){
-        
+    else if (gameStatus == "instructionsloaded") {
+
         firstClue();
-        
+
     }
-    else if (gameStatus == "firstclueloaded"){
-        
+    else if (gameStatus == "firstclueloaded") {
+
         secondClue();
-        
+
     }
-    else if (gameStatus == "secondclueloaded"){
-        
+    else if (gameStatus == "secondclueloaded") {
+
         thirdClue();
-        
+
     }
-    else if (gameStatus == "thirdclueloaded"){
-        
+    else if (gameStatus == "thirdclueloaded") {
+
         fourthClue();
-        
+
     }
-    else if (gameStatus == "answerlist"){
-        
+    else if (gameStatus == "answerlist") {
+
         checkAnswer();
-        
+
     }
-    
-    
+
+
 }
 
-function buttonBHandler(){
-    
-    if (gameStatus == "firstclueloaded" || gameStatus =="secondclueloaded" || gameStatus =="thirdclueloaded" || gameStatus =="fourthclueloaded"){
-        
+function buttonBHandler() {
+
+    if (gameStatus == "firstclueloaded" || gameStatus == "secondclueloaded" || gameStatus == "thirdclueloaded" || gameStatus == "fourthclueloaded") {
+
         getRandomCandidateAnswers();
-        
-    } 
-    
+
+    }
+
 }
-
-
-
-
