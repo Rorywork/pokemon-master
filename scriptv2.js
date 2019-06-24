99d8a76dadf3b9a191ea86a749294431f3de38b1
Rory Lindsay <rorywork95@outlook.com>
2019-06-24 13:27:42 +0000

    Rewrote if statement in modern syntax within clicked function

--- scriptv2.js
+++ scriptv2.js
@@ -29,23 +29,8 @@ document.getElementById('button1').addEventListener('click', clicked);
 function clicked(){
     
     let imageRequired = "front"
-    
-    console.log("Button Clicked")
     let elem = document.createElement("img");
     document.getElementById("image-placeholder").appendChild(elem);
     
-    if (imageRequired === "front"){
-        
-        elem.src = 'https://pbs.twimg.com/profile_images/653700295395016708/WjGTnKGQ_400x400.png';
-        
-    } else{
-        elem.src = 'https://cdn.pixabay.com/photo/2018/12/04/22/38/road-3856796__340.jpg';
-    }
-    
-
-    
-condition ? exprIfTrue : exprIfFalse 
-    
-    
-    
+    imageRequired === "front" ? elem.src = 'https://pbs.twimg.com/profile_images/653700295395016708/WjGTnKGQ_400x400.png' : elem.src = 'https://cdn.pixabay.com/photo/2018/12/04/22/38/road-3856796__340.jpg';
 }
