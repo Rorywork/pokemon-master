bb365298e35bb790a4eacc4cd68177ae1762a5d9
Rory Lindsay <rorywork95@outlook.com>
2019-06-24 13:34:43 +0000

    Created global variable gameState assigned it to off

--- scriptv2.js
+++ scriptv2.js
@@ -2,6 +2,10 @@
 
 // This is the Pokemon object which will be used in the program
 
+let gameState = "off";
+
+
+
 class Pokemon {
     constructor(id,name,frontImage, backImage, type, ability, weight, height) {
             this.id = id;
@@ -16,14 +20,15 @@ class Pokemon {
     getImageUrl() {
         return this.frontImage();
     }
+    getClue() {
+        return this.weight();
+    }
 }
 
 
 
 document.getElementById('button1').addEventListener('click', pokemonImage);
-
-
-
+document.getElementById('button2').addEventListener('click', clue);
 
 
 function pokemonImage(){
@@ -34,3 +39,13 @@ function pokemonImage(){
     
     imageRequired === "front" ? elem.src = 'https://pbs.twimg.com/profile_images/653700295395016708/WjGTnKGQ_400x400.png' : elem.src = 'https://cdn.pixabay.com/photo/2018/12/04/22/38/road-3856796__340.jpg';
 }
+
+
+function clue(){
+    
+    console.log("clicked");
+}
+
+
+
+
