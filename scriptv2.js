ae1fc1466aa6d123abfc2b325740f1afee1670d9
Rory Lindsay <rorywork95@outlook.com>
2019-06-24 13:07:07 +0000

    Created a clicked function - then used an event listener to test my button clicks - console log output successful

--- scriptv2.js
+++ scriptv2.js
@@ -1,6 +1,7 @@
 /* global $ */
 
 // This is the Pokemon object which will be used in the program
+
 class Pokemon {
     constructor(id,name,frontImage, backImage, type, ability, weight, height) {
             this.id = id;
@@ -15,45 +16,25 @@ class Pokemon {
     
     
     
+    
+    getImageUrl() {
+        return this.frontImage();
+    }
+    
+    
+    
+    
 }
 
 
 
+document.getElementById('button1').addEventListener('click', clicked);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    class Pokemon {
-        constructor(id,name,frontImage, backImage, type, ability, weight, height) {
-            this.id = id;
-            this.name = name;
-            this.frontImage = frontImage;
-            this.backImage = backImage;
-            this.type = type;
-            this.ability = ability;
-            this.weight = weight;
-            this.height = height;
-        }
+function clicked(){
+    
+    console.log("Button Click Successful")
+    
+}
