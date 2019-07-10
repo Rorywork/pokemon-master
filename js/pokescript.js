fb19da521f4294cc88bd7dded55da9b2feb40986
Rorywork <rorywork95@outlook.com>
2019-07-10 11:27:00 +0100

    Created modal with instructions for new players

--- js/pokescript.js
+++ js/pokescript.js
@@ -76,6 +76,7 @@ function buttonOnOffHandler() {
         // Reset variables
         gameState = "off";
         userScore = 0;
+        candidateAnswerPokemonNames = [];
         // Clear contents from screen
         document.getElementById("scoreScreen").innerHTML = "";
         document.getElementById("mainBody").innerHTML = "";
@@ -301,4 +302,32 @@ function buttonBHandler() {
         gameState = "selecting-answer"
     }
 
+}
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
